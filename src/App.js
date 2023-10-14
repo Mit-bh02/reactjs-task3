5979513f6132b02d96c1ef9941d7472549898fe9
Mit0921 <meetbhardiwala@gmail.com>
2023-10-14 20:11:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ColorPicker from './components/ColorPicker';
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <center><h1>Color Picker</h1></center>
+      <ColorPicker />
+    </div>
+  );
+}
+
+export default App;
